a4d6b3cdc28559e5702d6c346c15c06bd75f4643
ravul <ravul@PRAVEEN>
2020-05-10 21:38:54 +0530

    praveen

---
+++ java project/src/assingment/practise.java
@@ -0,0 +1,18 @@
+package assingment;
+
+public class practise {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		if(a==b) {
+			System.out.println("yes");
+		}if(a!=b){
+			System.out.println("no");
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
